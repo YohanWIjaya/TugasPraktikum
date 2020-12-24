0f8bb457e17e19877010f7e6c7ef7d5977d5f1f8
YohanWIjaya <75228962+YohanWIjaya@users.noreply.github.com>
2020-12-24 23:46:56 +0700

    week 14
    kumpu;

---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputan = new System.Windows.Forms.TextBox();
+            this.akhir = new System.Windows.Forms.TextBox();
+            this.awal = new System.Windows.Forms.TextBox();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputan
+            // 
+            this.inputan.Location = new System.Drawing.Point(169, 71);
+            this.inputan.Name = "inputan";
+            this.inputan.Size = new System.Drawing.Size(629, 22);
+            this.inputan.TabIndex = 0;
+            // 
+            // akhir
+            // 
+            this.akhir.Location = new System.Drawing.Point(568, 142);
+            this.akhir.Name = "akhir";
+            this.akhir.Size = new System.Drawing.Size(230, 22);
+            this.akhir.TabIndex = 1;
+            // 
+            // awal
+            // 
+            this.awal.Location = new System.Drawing.Point(169, 144);
+            this.awal.Name = "awal";
+            this.awal.Size = new System.Drawing.Size(230, 22);
+            this.awal.TabIndex = 2;
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(106, 442);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(0, 17);
+            this.labelOutput.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(629, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "konvert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "masukan kalimat";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "masukan huruf";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 442);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "hasil";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(505, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "menjadi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 706);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.awal);
+            this.Controls.Add(this.akhir);
+            this.Controls.Add(this.inputan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputan;
+        private System.Windows.Forms.TextBox akhir;
+        private System.Windows.Forms.TextBox awal;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            inputan.CharacterCasing = CharacterCasing.Upper;
+            char[] input = inputan.Text.ToCharArray();
+            char[] alpha1 = awal.Text.ToCharArray();
+            char[] alpha2 = akhir.Text.ToCharArray();
+            char alfa1 = Convert.ToChar(awal.Text);
+            char alfa2 = alpha2[0];
+            int range = alfa2 - alfa1;
+            string hasil = " ";
+            char spasi1 = '/';
+            char space = ' ';
+            string huruf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            char[] charhuruf = huruf.ToCharArray();
+            if (range < 0)
+            {
+                range += 26 ;
+            }
+            for (int i = 0 ; i < inputan.Text.Length ; i++)
+            {
+                for (int j = 0 ; j < 26 ; j++)
+                {
+                    if (charhuruf[j] == input[i])
+                    {
+                        int baru = j + range;
+                        if (baru >= 26)
+                        {
+                            baru -= 26;
+                        }
+                        input[i] = charhuruf[baru];
+                        hasil += input[i];                       
+                        input[i] = spasi1;
+                    }
+                    if (input[i] == space)
+                    {
+                        hasil   += " ";                                           
+                        input[i] = spasi1;
+                    }
+                }
+            }
+            labelOutput.Text = hasil;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+        
+    
+
